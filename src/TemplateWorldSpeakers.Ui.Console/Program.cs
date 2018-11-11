58d8a23e3f785a61b42df853a1beae3d9fd43599
delagetto <delagetto@users.noreply.github.com>
2018-11-11 21:53:20 +0700

    created required skeleton projects

---
+++ src/TemplateWorldSpeakers.Ui.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateWorldSpeakers.Ui.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
