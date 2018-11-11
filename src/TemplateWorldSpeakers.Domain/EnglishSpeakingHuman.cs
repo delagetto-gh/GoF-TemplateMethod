fdb95649a1d5b4114651935513b0788eb8df02b7
delagetto <delagetto@users.noreply.github.com>
2018-11-11 23:33:34 +0700

    created first test, currently failing.. time to refactor

---
+++ src/TemplateWorldSpeakers.Domain/EnglishSpeakingHuman.cs
@@ -0,0 +1,17 @@
+namespace TemplateWorldSpeakers.Domain
+{
+    public class EnglishSpeakingHuman
+    {
+        private string name;
+
+        public EnglishSpeakingHuman(string name)
+        {
+            this.name = name;
+        }
+
+        public string Greet()
+        {
+            return "";
+        }
+    }
+}
--- tests/TemplateWorldSpeakers.Domain.Tests/TemplateWorldSpeakerTests.cs
+++ tests/TemplateWorldSpeakers.Domain.Tests/TemplateWorldSpeakerTests.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using TemplateWorldSpeakers.Domain;
 
 namespace TemplateWorldSpeakers.Domain.Tests
 {
     public class TemplateWorldSpeakerTests
     {
-        [Fact]
+        // Nunit style SetUp's are achieved through the constructor (no explicit [Setup] method needed!)
+        public TemplateWorldSpeakerTests()
+        {
+        }
+
+        [Fact] //The [Fact] attribute indicates a test method that is run by the test runner
         public void ShouldReturnCompleteEnglishTransationHelloSentanceGivenNameAde()
         {
+            //arrange
+            var sut = new EnglishSpeakingHuman ("Ade");
 
+            //act
+            var result = sut.Greet();
+
+            //assert
+            Assert.Equal("Hello, my name is Ade!", result);
         }
     }
 }
