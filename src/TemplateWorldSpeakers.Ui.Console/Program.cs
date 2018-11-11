7d74adf985d538aa113170a37cd4b092071928d7
delagetto <delagetto@users.noreply.github.com>
2018-11-11 22:36:41 +0700

    console suffixed namespace was taking prescedence over System.Console.Writeline :(

--- src/TemplateWorldSpeakers.Ui.Console/Program.cs
+++ src/TemplateWorldSpeakers.Ui.Console/Program.cs
@@ -6,7 +6,7 @@ namespace TemplateWorldSpeakers.Ui.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
